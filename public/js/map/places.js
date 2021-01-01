7616e3b29cac1f2978aca425612ae0589ae2bfbd
venkat9507 <70470427+venkat9507@users.noreply.github.com>
2021-01-01 20:55:14 +0530

    first commit

---
+++ public/js/map/places.js
@@ -0,0 +1,11 @@
+﻿/*
+place ={
+		infowin_html: content of infowindow,
+		tooltip_html: content of tooltip,
+		position:{lat:latitude, lng:longitude}
+		
+}
+*/
+var places =Array();
+places.push({infowin_html:"<h4><a href='#' style='text-decoration:none; color:#094051;'>Arulraj Hospital</a></h4><div><img class='floatleft' style='margin:5px 0 5px 0;' src='http://www.arulsenvironment.com/images/about.jpg' width='80' hieght='120' /><h5 style='padding-top:10px;margin-left:10px;'>Dr.Arulraj</h5></div><div></div>", tooltip_html:"<h4 style='padding-bottom:5px;font-weight:bold;'>ARULRAJ HOSPITAL</h4><div>145/5B, Jeyaraj Road<br>Tuticorin.</div>", position:{lat:8.808372, lng:78.139873}});
+
